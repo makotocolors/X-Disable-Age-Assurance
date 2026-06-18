c1af047087afe0a68ec672eb5615b82dd48cfd9b
Makoto <colorsmakoto@gmail.com>
2026-06-18 02:31:12 -0300

    Format user script metadata for consistency

--- script.user.js
+++ script.user.js
@@ -1,17 +1,17 @@
 // ==UserScript==
-// @name         X Disable Age Assurance
-// @namespace    https://github.com/makotocolors
-// @version      1.0.0
-// @description  Userscript that disables the age assurance flow on X (Twitter).
-// @author       Makoto
-// @homepageURL  https://github.com/makotocolors/X-Disable-Age-Assurance
-// @supportURL   https://github.com/makotocolors/X-Disable-Age-Assurance/issues
-// @match        https://x.com/*
-// @match        https://twitter.com/*
-// @run-at       document-start
-// @grant        unsafeWindow
-// @updateURL    https://raw.githubusercontent.com/makotocolors/X-Disable-Age-Assurance/main/script.user.js
-// @downloadURL  https://raw.githubusercontent.com/makotocolors/X-Disable-Age-Assurance/main/script.user.js
+// @name                   X Disable Age Assurance
+// @namespace              https://github.com/makotocolors
+// @version                1.0.0
+// @description            Userscript that disables the age assurance flow on X (Twitter).
+// @author                 Makoto
+// @homepageURL            https://github.com/makotocolors/X-Disable-Age-Assurance
+// @supportURL             https://github.com/makotocolors/X-Disable-Age-Assurance/issues
+// @match                  https://x.com/*
+// @match                  https://twitter.com/*
+// @run-at                 document-start
+// @grant                  unsafeWindow
+// @updateURL              https://raw.githubusercontent.com/makotocolors/X-Disable-Age-Assurance/main/script.user.js
+// @downloadURL            https://raw.githubusercontent.com/makotocolors/X-Disable-Age-Assurance/main/script.user.js
 // ==/UserScript==
 
 (() => {
