390c74ea6dee288af5b77b9df19eb7a595b0c6d3
Makoto <colorsmakoto@gmail.com>
2026-06-20 11:33:59 -0300

    Bump version to 1.3.0 and update unsafeWindow reference

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                   X Disable Age Assurance
 // @namespace              https://github.com/makotocolors
-// @version                1.2.0
+// @version                1.3.0
 // @description            Userscript that disables the age assurance flow on X (Twitter).
 // @author                 Makoto
 // @homepageURL            https://github.com/makotocolors/X-Disable-Age-Assurance
@@ -14,8 +14,9 @@
 // @downloadURL            https://raw.githubusercontent.com/makotocolors/X-Disable-Age-Assurance/main/script.user.js
 // ==/UserScript==
 
+const pageWindow = typeof unsafeWindow !== "undefined"? unsafeWindow: window;
 const patcher = () => {
-    const override = unsafeWindow.__INITIAL_STATE__?.featureSwitch?.customOverrides;
+    const override = pageWindow.__INITIAL_STATE__?.featureSwitch?.customOverrides;
     if (override) {
         override.rweb_age_assurance_flow_enabled = false;
         return true;
