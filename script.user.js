87d057578e534adee83840a24e3979f7a48767d9
Makoto <colorsmakoto@gmail.com>
2026-06-23 18:49:31 -0300

    Bump version to 1.4.0 and refactor patcher function

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                   X Disable Age Assurance
 // @namespace              https://github.com/makotocolors
-// @version                1.3.0
+// @version                1.4.0
 // @description            Userscript that disables the age assurance flow on X (Twitter).
 // @author                 Makoto
 // @homepageURL            https://github.com/makotocolors/X-Disable-Age-Assurance
@@ -14,14 +14,20 @@
 // @downloadURL            https://raw.githubusercontent.com/makotocolors/X-Disable-Age-Assurance/main/script.user.js
 // ==/UserScript==
 
-const pageWindow = typeof unsafeWindow !== "undefined"? unsafeWindow: window;
-const patcher = () => {
-    const override = pageWindow.__INITIAL_STATE__?.featureSwitch?.customOverrides;
+"use strict";
+{
+  const win = typeof unsafeWindow !== "undefined"? unsafeWindow: window;
+  const patch = () => {
+    const override = win.__INITIAL_STATE__?.featureSwitch?.customOverrides;
     if (override) {
-        override.rweb_age_assurance_flow_enabled = false;
-        return true;
-    };
-};
+      override.rweb_age_assurance_flow_enabled = false;
+      return true;
+    } else return false;
+  };
 
-const observer = new MutationObserver(() => patcher() && observer.disconnect());
-patcher() || (observer.observe(document.documentElement, { childList: true, subtree: true }), setTimeout(() => observer.disconnect(), 3000));
+  if (!patch()) {
+    const observer = new MutationObserver(() => patch() && observer.disconnect());
+    observer.observe(document.documentElement, { childList: true, subtree: true });
+    setTimeout(() => observer.disconnect(), 3000);
+  };
+};
